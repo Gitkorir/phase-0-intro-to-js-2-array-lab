65d69d8d33acc2ede8f82d72dab4daaf878b697f
Arnold Korir <arnoldkorir201@gmail.com>
2025-02-28 15:07:28 +0300

    creates the index.js file structure & implements the test files instructions

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+//initial cats array
+let cats = ["Milo", "Otis", "Garfield"];
+//destructivelyAppendCat(name)
+function destructivelyAppendCat(name) {
+  cats.push("Ralph");
+}
+
+//destructivelyPrependCat(name)
+function destructivelyPrependCat(name) {
+  cats.unshift("Bob");
+}
+
+//destructivelyRemoveLastcat(name)
+function destructivelyRemoveLastCat(name) {
+  cats.pop("Otis");
+}
+
+//destructivelyRemoveFirstCat(name)
+function destructivelyRemoveFirstCat(name) {
+  cats.shift("Milo");
+}
+//appendsCat(name)
+function appendCat(name) {
+  return [...cats, "Broom"];
+}
+//prependCat(name)
+function prependCat(name) {
+  return ["Arnold", ...cats];
+}
+//removeLastCat()
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+//removeFirstCat()
+function removeFirstCat() {
+  return cats.slice(1);
+}
+if (typeof mudule !== undefined && module.exports) {
+  module.exports = {
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveLastCat,
+    destructivelyRemoveFirstCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat,
+  };
+}
